43b8d05add18cebbb3c38f65b0c7d6d113aa919b
Kevinn0809 <kpinzonnino@gmail.com>
2023-10-13 19:25:44 -0500

    creación del navbar

--- proyecto/front/src/app/app.module.ts
+++ proyecto/front/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavComponent } from './components/barra-nav/barra-nav.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        BarraNavComponent,
+
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
