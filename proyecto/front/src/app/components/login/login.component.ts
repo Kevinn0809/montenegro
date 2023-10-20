d71f433259a709bcfbe6b524cacaca8bbb1d8897
Kevinn0809 <kpinzonnino@gmail.com>
2023-10-20 16:52:50 -0500

    integracion rama de Kevin

--- proyecto/front/src/app/components/login/login.component.ts
+++ proyecto/front/src/app/components/login/login.component.ts
@@ -51,70 +51,62 @@ export class LoginComponent implements OnInit {
 
     registerUser() {
         if (this.idUsuarioUrl == null) {
-            if (this.confirmPass()) {
-                this._usuarioService.postUsuario(this.formRegister.value).subscribe(data => {
-                    let timerInterval: any
-                    let b: any
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Usuario creado',
-                        timer: 2000,
-                        timerProgressBar: true,
-                        didOpen: () => {
-                            b = Swal.getHtmlContainer()!.querySelector('b');
-                            timerInterval = setInterval(() => {
-                                const timerLeft = Swal.getTimerLeft();
-                                if (b) {
-                                    b.textContent = timerLeft!.toString();
-                                }
-                            }, 100);
-                        },
-                        willClose: () => {
-                            clearInterval(timerInterval);
-                        }
-                    }).then((result) => {
-                        if (result.dismiss === Swal.DismissReason.timer) {
-                            console.log('Se acabo el tiempo, por ende cierro el modal');
-                        } else if (result.isConfirmed) {
-                            console.log('El usuario confirmó el modal');
-                        }
-                    });
-                    console.log(data)
-                    this.resetform()
-                    this.cerrarBtnRegistro.nativeElement.click()
+            this._usuarioService.postUsuario(this.formRegister.value).subscribe(data => {
+                let timerInterval: any
+                let b: any
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Usuario creado',
+                    timer: 2000,
+                    timerProgressBar: true,
+                    didOpen: () => {
+                        b = Swal.getHtmlContainer()!.querySelector('b');
+                        timerInterval = setInterval(() => {
+                            const timerLeft = Swal.getTimerLeft();
+                            if (b) {
+                                b.textContent = timerLeft!.toString();
+                            }
+                        }, 100);
+                    },
+                    willClose: () => {
+                        clearInterval(timerInterval);
+                    }
+                }).then((result) => {
+                    if (result.dismiss === Swal.DismissReason.timer) {
+                        console.log('Se acabo el tiempo, por ende cierro el modal');
+                    } else if (result.isConfirmed) {
+                        console.log('El usuario confirmó el modal');
+                    }
+                });
+                console.log(data)
+                this.resetform()
+                this.cerrarBtnRegistro.nativeElement.click()
 
-                })
-            } else {
-                console.log('ayuda')
-            }
+            })
         } else {
-            if (this.confirmPass()) {
-                this._usuarioService.putEditarUsuario(this.idUsuarioUrl, this.formRegister.value).subscribe(data => {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Usuario Actualizado',
-                        iconColor: '#2ce30b'
-                    })
-                    this.router.navigate(['/admin/usuarios-registrados'])
+            this._usuarioService.putEditarUsuario(this.idUsuarioUrl, this.formRegister.value).subscribe(data => {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Usuario Actualizado',
+                    iconColor: '#2ce30b'
                 })
-            } else {
-                console.log('%c Contraseña invalida!!', 'color: red; font-size:2rem;')
-            }
+                this.router.navigate(['/admin/usuarios-registrados'])
+            })
         }
     }
 
-    confirmPass() {
-        let passUser = this.formRegister.get('password')?.value
-        if (passUser != this.passConfirm.nativeElement.value) {
-            this.alertPass.nativeElement.classList.remove('d-none')
-            this.submitRegister.nativeElement.disabled = "true"
-            return true
-        } else {
-            this.alertPass.nativeElement.classList.add('d-none')
-            this.submitRegister.nativeElement.disabled = "false"
-            return false
-        }
-    }
+    // confirmPass() {
+    //     let passUser = this.formRegister.get('password')?.value
+    //     if (passUser != this.passConfirm.nativeElement.value) {
+    //         this.alertPass.nativeElement.classList.remove('d-none')
+    //         this.submitRegister.nativeElement.disabled = "true"
+    //         return true
+    //     } else {
+    //         this.alertPass.nativeElement.classList.add('d-none')
+    //         this.submitRegister.nativeElement.disabled = "false"
+    //         return false
+    //     }
+    // }
 
     IngresoUsuario() {
         this._usuarioService.postIngresocuenta(this.Userformlogin).subscribe(respuestaApi => {
